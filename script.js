38bcad22ce86476204f480aff667ca498bf6d425
FernandoNB <111037499+FernandoNB@users.noreply.github.com>
2025-01-16 13:15:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('form');
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        alert('Formulário enviado com sucesso!');
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('form');
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        alert('Formulário enviado com sucesso!');
+    });
+});
